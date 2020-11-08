ed483168d1e92c9b53b165f19060096054ccbd89
Margarita Shimanskaia <shimanskayamargarita@gmail.com>
2020-11-08 21:27:14 +0300

    Add files via upload

---
+++ insql.php
@@ -0,0 +1,8 @@
+<?php
+  $host_name  = "localhost";
+  $database   = "mydb"; 
+  $user_name  = "rita";
+  $password   = "delraY#";
+  $conn=mysqli_connect($host_name, $user_name, $password, $database);
+
+?>
